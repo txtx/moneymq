46bf0c40d89d63159feff5c537ade67b6e261382
MicaiahReid <micaiahreid@gmail.com>
2025-11-05 12:05:22 -0500

    feat: update token account amount and add debug logging

--- crates/core/src/validator/surfnet_utils.rs
+++ crates/core/src/validator/surfnet_utils.rs
@@ -1,6 +1,7 @@
 use serde_json::json;
 use solana_client::{rpc_client::RpcClient, rpc_request::RpcRequest};
 use solana_keypair::Pubkey;
+use tracing::debug;
 
 /// Sets account data for a given account on the Surfnet local validator
 pub fn surfnet_set_account(rpc_client: &RpcClient, pubkey: &Pubkey) -> Result<(), String> {
@@ -28,7 +29,7 @@ pub fn surfnet_set_token_account(
     token_program: &Pubkey,
 ) -> Result<(), String> {
     let account_data = json!({
-        "amount": 1_000_000,
+        "amount": 1_000_000_000,
     });
     let params = json!([
         pubkey.to_string(),
@@ -36,6 +37,8 @@ pub fn surfnet_set_token_account(
         account_data,
         token_program.to_string()
     ]);
+
+    debug!("Setting token account with payload: {:?}", params);
 
     let _ = rpc_client
         .send::<serde_json::Value>(
